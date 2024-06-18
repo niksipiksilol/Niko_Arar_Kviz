42747c09dcf68b2c0088e24ae1244841a89bbce4
niksilolic <nikormitbuba@gmail.com>
2024-06-18 15:55:06 +0200

    Završen kompletno projekt!

--- Form4.Designer.cs
+++ Form4.Designer.cs
@@ -37,9 +37,10 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(107, 168);
+            this.button1.Location = new System.Drawing.Point(160, 258);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(210, 90);
+            this.button1.Size = new System.Drawing.Size(315, 138);
             this.button1.TabIndex = 0;
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
@@ -47,9 +48,10 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(454, 294);
+            this.button2.Location = new System.Drawing.Point(681, 452);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(210, 90);
+            this.button2.Size = new System.Drawing.Size(315, 138);
             this.button2.TabIndex = 1;
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
@@ -57,9 +59,10 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(454, 168);
+            this.button3.Location = new System.Drawing.Point(681, 258);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(210, 90);
+            this.button3.Size = new System.Drawing.Size(315, 138);
             this.button3.TabIndex = 2;
             this.button3.Text = "button3";
             this.button3.UseVisualStyleBackColor = true;
@@ -67,9 +70,10 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(107, 294);
+            this.button4.Location = new System.Drawing.Point(160, 452);
+            this.button4.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(210, 90);
+            this.button4.Size = new System.Drawing.Size(315, 138);
             this.button4.TabIndex = 3;
             this.button4.Text = "button4";
             this.button4.UseVisualStyleBackColor = true;
@@ -78,23 +82,25 @@
             // pitanje
             // 
             this.pitanje.AutoSize = true;
-            this.pitanje.Font = new System.Drawing.Font("Microsoft Sans Serif", 38.25F);
-            this.pitanje.Location = new System.Drawing.Point(107, 59);
+            this.pitanje.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.pitanje.Location = new System.Drawing.Point(160, 91);
+            this.pitanje.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.pitanje.Name = "pitanje";
-            this.pitanje.Size = new System.Drawing.Size(179, 59);
+            this.pitanje.Size = new System.Drawing.Size(143, 46);
             this.pitanje.TabIndex = 4;
             this.pitanje.Text = "Pitanje";
             // 
             // Form4
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1200, 692);
             this.Controls.Add(this.pitanje);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form4";
             this.Text = "Izbornik kvizova";
             this.ResumeLayout(false);
